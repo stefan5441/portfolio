6d8801549bd74427f2f3dcd6388921ed7f0cb38f
Stefan Chambov <stefan.cambov2000@gmail.com>
2025-07-23 19:35:40 +0200

    BE: Removing the github part

--- portfolio-be/src/index.ts
+++ portfolio-be/src/index.ts
@@ -1,18 +1,13 @@
-import * as dotenv from "dotenv";
 import axios, { AxiosError } from "axios";
 import express, { Request, Response } from "express";
 
 const app = express();
-dotenv.config();
 const PORT = 3000;
 
 const LEETCODE_URL = "https://leetcode.com/graphql";
 const CODEFORCES_URL = "https://codeforces.com/api/user.status";
-const GITHUB_URL = "https://api.github.com/graphql";
 
 app.use(express.json());
-
-// TODO: Check the levels stats
 
 app.get("/activity/leetcode", async (req: Request, res: Response) => {
   const username = req.query.username as string;
@@ -98,66 +93,6 @@ app.get("/activity/codeforces", async (req: Request, res: Response) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Failed to fetch Codeforces activity" });
-  }
-});
-
-app.get("/activity/github", async (req: Request, res: Response) => {
-  const username = req.query.username as string;
-  if (!username) {
-    return res.status(400).json({ error: "Username query param is required" });
-  }
-
-  const query = `
-    query ($login: String!) {
-      user(login: $login) {
-        contributionsCollection {
-          contributionCalendar {
-            weeks {
-              contributionDays {
-                date
-                contributionCount
-              }
-            }
-          }
-        }
-      }
-    }
-  `;
-
-  try {
-    const response = await axios.post(
-      GITHUB_URL,
-      {
-        query,
-        variables: { login: username },
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.GITHUB_PAT_TOKEN}`,
-          "Content-Type": "application/json",
-          "User-Agent": "activity-fetcher",
-        },
-      }
-    );
-
-    const weeks = response.data.data.user.contributionsCollection.contributionCalendar.weeks;
-    const contributions = weeks.flatMap((week: any) => week.contributionDays);
-
-    const result = contributions.map(({ date, contributionCount }: any) => {
-      const c = contributionCount;
-      let level = 0;
-      if (c >= 1 && c <= 2) level = 1;
-      else if (c <= 5) level = 2;
-      else if (c <= 10) level = 3;
-      else if (c >= 11) level = 4;
-
-      return { date, count: c, level };
-    });
-
-    res.json(result);
-  } catch (error) {
-    console.error((error as AxiosError)?.response?.data || error);
-    res.status(500).json({ error: "Failed to fetch GitHub activity" });
   }
 });
 
